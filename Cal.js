9602abe5eae4107ba858c6173d6e896077ddeeea
nguyentiendung1997 <dungnt@sphinx-software.com>
2019-03-22 01:07:29 +0700

    This is my homework

---
+++ Cal.js
@@ -0,0 +1,28 @@
+class Calculator {
+  constructor(a, b); {
+
+}
+
+class add(a, b) {
+  return a + b;
+}
+
+class subtract(a, b) {
+  return a - b;
+}
+
+class multiply(a, b) {
+  return a * b;
+}
+
+class divide(a, b) {
+  if (b == 0) {
+       console.log("Error");
+  } else
+  return x / y;
+}
+  
+  const Calculator = new Calculator();
+  console.log(Calculator.add(2,3));
+  }
+}
